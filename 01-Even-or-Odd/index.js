f653e19fa9e7a03b7fff1bb02c653e6e3c46711f
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-15 13:10:31 +0530

    Add files via upload

---
+++ 01-Even-or-Odd/index.js
@@ -0,0 +1,10 @@
+// to check if a number is even or odd
+
+let num = 12547852;
+
+if (num % 2 === 0) {
+    console.log("it is an even number");
+}
+else {
+    console.log("it is an odd number");
+}
