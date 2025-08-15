41a333e902a33fc8b4b142de190da90014e3ba39
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-15 13:14:01 +0530

    Add files via upload

---
+++ 02-Positive-Negative-or-zero/index.js
@@ -0,0 +1,15 @@
+// to check whether the number is positive, negative, or zero
+
+let num = 0;
+
+if (num < 0) {
+    console.log("It is a negative number");
+}
+
+else if (num === 0) {
+    console.log("It is a zero");
+}
+
+else {
+    console.log("It is a positive number");
+}
