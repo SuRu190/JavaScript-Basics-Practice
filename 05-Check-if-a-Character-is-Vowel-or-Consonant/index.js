236387f99242db2ede02569e0eeb40d398ef3718
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-15 13:20:58 +0530

    Add files via upload

---
+++ 05-Check-if-a-Character-is-Vowel-or-Consonant/index.js
@@ -0,0 +1,10 @@
+// to check if a character is vowel or consonant
+
+let alphabet = "s";
+
+if (alphabet === "a" || alphabet === "e" || alphabet === "i" || alphabet === "o" || alphabet === "u") {
+    console.log("It is a vowel");
+}
+else {
+    console.log("It is a consonant");
+}
