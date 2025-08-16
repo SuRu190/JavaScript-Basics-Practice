16015918c77b2db377f54746ef4df3f031b241be
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-16 13:04:05 +0530

    Add files via upload

---
+++ 10-Factorial-of-a-Number/index.js
@@ -0,0 +1,10 @@
+// to find the factorial of a number
+
+let number = 5;
+let fact = 1;
+
+for (let i = 1; i <= number; i++) {
+    fact = fact * i;
+}
+
+console.log("factorial of: " + number + " is " + fact);
