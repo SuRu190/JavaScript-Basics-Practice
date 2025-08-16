7ece32fbf70c8f407f6cc378114c13878eae0e89
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-16 13:03:35 +0530

    Add files via upload

---
+++ 08-Check-the-Number-is-Divisible-by-2-and-3/index.js
@@ -0,0 +1,10 @@
+// to check a number whether it is divisible by both 2 and 3
+
+let num = 24;
+
+if (num % 2 === 0 && num % 3 === 0) {
+    console.log("Yes it is divisble by both 2 and 3");
+}
+else {
+    console.log("Not divisible by 2 and 3");
+}
