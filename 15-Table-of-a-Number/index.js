59956c64b07ae842b845bbbf204be254e2fc00ae
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-18 18:36:06 +0530

    Add files via upload

---
+++ 15-Table-of-a-Number/index.js
@@ -0,0 +1,7 @@
+// Taking number from user and print its multiplication tables
+
+let number = prompt("Enter the number:");
+
+for (let i = 1; i <= 10; i++) {
+    console.log(number + " x " + i + " = " + (number * i)); 
+}
