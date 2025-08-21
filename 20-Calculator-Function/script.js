0dfbfe702c5f5e075619450f8c129362eaccd019
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-21 12:41:49 +0530

    Create script.js

---
+++ 20-Calculator-Function/script.js
@@ -0,0 +1,21 @@
+// Create functions for addition, subtraction, multiplication, and modulus of two numbers.
+
+function add(x, y) {
+    return x + y;
+}
+console.log(add(10, 3));
+
+function subtract(x, y) {
+    return x - y;
+}
+console.log(subtract(10, 3));
+
+function multiply(x, y) {
+    return x * y;
+}
+console.log(multiply(10, 3));
+
+function modulus(x, y) {
+    return x % y;
+}
+console.log(modulus(10, 3));
