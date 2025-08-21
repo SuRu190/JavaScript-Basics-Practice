ab095fcfce464047d769fcf655bb141e752acc87
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-21 12:50:39 +0530

    Create script.js

---
+++ 26-Print-Numbers/script.js
@@ -0,0 +1,11 @@
+// Print numbers from 1 to 10 using for and while loops.
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+let j = 1;
+while (j <= 10) {
+    console.log(j);
+    j++;
+}
