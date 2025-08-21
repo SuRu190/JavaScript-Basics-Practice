09bdbe69ababbc61bd78b99c6f221c0ada7e2cb8
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-21 12:39:23 +0530

    Create script.js

---
+++ 17-Comparison/script.js
@@ -0,0 +1,8 @@
+// Compare two values using == and ===. Also check a condition using logical operators.
+
+let a = 5;
+let b = "5";
+
+console.log(a == b);   // true
+console.log(a === b);  // false
+console.log(a > 3 && b < 10); // true
