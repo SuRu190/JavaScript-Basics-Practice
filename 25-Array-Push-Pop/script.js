fd9df4f1e8c92491c72d2f130632c4690e855e9a
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-21 12:47:57 +0530

    Create script.js

---
+++ 25-Array-Push-Pop/script.js
@@ -0,0 +1,10 @@
+// Perform push, pop, shift, and unshift operations on an array of fruits.
+
+let fruits = ["apple", "banana", "orange"];
+fruits.push("mango"); 
+fruits.unshift("grape"); 
+console.log(fruits);
+
+fruits.pop(); 
+fruits.shift(); 
+console.log(fruits);
