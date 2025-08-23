55ee42a17a129cfd948b61cb6b0b15c8470cb55c
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-23 11:17:04 +0530

    Update script.js

--- 23-Basic-Loops/script.js
+++ 23-Basic-Loops/script.js
@@ -1,15 +1,18 @@
 // Print numbers from 1 to 5 using for, while, and do-while loops.
 
+// using for loop
 for (let i = 1; i <= 5; i++) {
     console.log(i);
 }
 
+// using while loop
 let j = 1;
 while (j <= 5) {
     console.log(j);
     j++;
 }
 
+// using do-while loop
 let k = 1;
 do {
     console.log(k);
