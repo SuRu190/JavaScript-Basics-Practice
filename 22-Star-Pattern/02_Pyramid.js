e27a02df57014151b2209a1153813893262ccc44
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-26 13:55:48 +0530

    Create 02_Pyramid.js

---
+++ 22-Star-Pattern/02_Pyramid.js
@@ -0,0 +1,14 @@
+// Q17: Print pyramid of stars
+//     *  
+//    *** 
+//   *****
+//  *******
+// *********
+
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
+    let spaces = " ".repeat(n - i);
+    let stars = "*".repeat(2 * i - 1);
+    console.log(spaces + stars);
+}
