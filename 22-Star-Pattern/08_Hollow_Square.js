541a0100f920a4b918d970f893835b010191b0b4
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-26 13:57:30 +0530

    Create 08_Hollow_Square.js

---
+++ 22-Star-Pattern/08_Hollow_Square.js
@@ -0,0 +1,20 @@
+// Q23: Print hollow square pattern
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
+    let line = "";
+    for (let j = 1; j <= n; j++) {
+        if (i === 1 || i === n || j === 1 || j === n) {
+            line += "*";
+        } else {
+            line += " ";
+        }
+    }
+    console.log(line);
+}
